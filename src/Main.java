2f25b8e5fa2aaebdb413b4b72ec51009d9608053
d1gypro <d1gypro@yandex.ru>
2023-07-19 21:21:04 +0300

    del .idea

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 public class Main {
+
     public static void main(String[] args) {
 
         OnTaskDoneListener listener = System.out::println;
@@ -9,5 +10,4 @@ public class Main {
 
         worker.start();
     }
-
 }
